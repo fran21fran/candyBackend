f1a8dcd49bd2c03f118d242399d817dfa60f70bf
fran21fran <cuenta4boludear@gmail.com>
2025-09-28 18:10:35 -0300

    Add files via upload

---
+++ backend/src/db.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as schema from "./schema";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error(
+    "DATABASE_URL must be set. Did you forget to provision a database?",
+  );
+}
+
+// Disable prefetch as it is not supported for "Transaction" pool mode in Supabase
+export const client = postgres(process.env.DATABASE_URL, { prepare: false });
+export const db = drizzle({ client, schema });
